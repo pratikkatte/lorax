5f8a142c6f6032c4f96b637ed280a58b113c1e6d
asaboowala <alisha3@gmail.com>
2024-10-15 13:03:34 -0700

    Merged HITL code with chat interface function

--- src/langgraph_tskit_hitl_basic.py
+++ src/langgraph_tskit_hitl_basic.py
@@ -1,6 +1,6 @@
 import getpass
 import os
-
+import sys
 from faiss_vector import get_vector_store, repo_to_text
 from tools import generator_tool
 from graph import create_graph
@@ -9,52 +9,73 @@ from utils import execute_generated_code
 print("here")
 
 
-def call_code_generator(question):
+vector_store = get_vector_store()
+retriever = vector_store.as_retriever(
+    search_type="similarity",  # Also test "similarity", "mmr"
+    search_kwargs={"k": 5})
 
-    vector_store = get_vector_store()
-    retriever = vector_store.as_retriever(
-        search_type="similarity",  # Also test "similarity", "mmr"
-        search_kwargs={"k": 5})
+code_generator = generator_tool()
 
-    code_generator = generator_tool()
-
-    app = create_graph()
-
+def call_code_generator(app, question):
     solution = app.invoke({"messages": [("user", question)], "iterations": 0, "error": "", "code_generator":code_generator,"retriever": retriever})
-
     llm_output = solution['generation']
 
     return llm_output
 
 
-query = "Calculate the diversity of the given treesequence."
-llm_output = call_code_generator(query)
+def chat_interface():
+    print("Tree-sequence analysis")
+    print("Type 'exit' to end the conversation.")
+    
+    while True:
+         # Example: Calculate the diversity of the given treesequence.
+        user_input = input("You: ").strip()
 
-print(llm_output.code)
-
-hitl_output = input("Please view above code and type 'Y' if approved and 'N' if not approved: ")
-
-# While loop to check if code is approved, breaks out once HITL approves of code
-while hitl_output not in ['Y', 'y', 'Yes', 'yes']:
-
-    if hitl_output in ['N', 'n', 'No', 'no']:
-        print("Code not approved, calling code generator again")
-
-        if "Code not approved, calling code generator again" not in query:
-            query += "Code not approved by human-in-the-loop, try again"
-
-        llm_output = call_code_generator(query)
-        print(llm_output.code)
-    else:
-        print("Invalid input, try again")
+        # Exit condition        
+        if user_input.lower() == 'exit':
+            print("Goodbye!")
+            sys.exit()
         
-    hitl_output = input("Please view above code and type 'Y' if approved and 'N' if not approved: ")
+        app = create_graph()
+        # response = f"Bot: You said: {user_input}"
+        # print(response)
+        # solution = app.invoke({"messages": [("user", user_input)], "iterations": 0, "error": "", "code_generator":code_generator,"retriever": retriever})
+
+        llm_output = call_code_generator(app, user_input)
+        print(llm_output.code)
 
 
-print("Code approved")
+        # Human in the Loop (HITL) Yes/No feedback
+        hitl_output = input("Please view above code and type 'Y' if approved and 'N' if not approved: ").lower()
+
+        # While loop to check if code is approved, breaks out once HITL approves of code
+        while hitl_output not in ['y', 'yes']:
+
+            if hitl_output in ['n', 'no']:
+                print("Code not approved, calling code generator again")
+
+                if "Code not approved, calling code generator again" not in user_input:
+                    user_input += "Code not approved by human-in-the-loop, try again"
+
+                # Regenerate code if not approved by HITL
+                llm_output = call_code_generator(app, user_input)
+                print(llm_output.code)
+            else:
+                print("Invalid input, try again")
+                
+            hitl_output = input("Please view above code and type 'Y' if approved and 'N' if not approved: ").lower()
+
+        print("Code approved")
 
 
-result = execute_generated_code(llm_output, "../data/basics.trees")
-print(result)
+        response = execute_generated_code(llm_output, "../data/sample.trees")
+        print(response)
+
+
+if __name__== "__main__":
+    chat_interface()
+
 
 # repo_to_text("../../tskit", 'data.text')
+
+
