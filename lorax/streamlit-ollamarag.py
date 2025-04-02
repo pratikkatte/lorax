be2d79598fedbdbd915c6df52233fd632e856551
asaboowala <alisha3@gmail.com>
2025-04-02 13:56:43 -0700

    some optimization and bug fixes

--- lorax/streamlit-ollamarag.py
+++ lorax/streamlit-ollamarag.py
@@ -18,11 +18,12 @@ from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import character, base
 
 # Define Variables
-DOC_PATH = "lorax/data-new.txt"
+DOC_PATH = "./data-new.txt"
 MODEL_NAME = "llama3.2"
 EMBEDDING_MODEL = "nomic-embed-text"
 VECTOR_STORE_NAME = "simple-rag"
 PERSIST_DIRECTORY = './tskit-vector'
+
 
 # ## Load text file using Langchain Text Loader
 # def ingest_text(doc_path):
@@ -53,73 +54,54 @@ def read_document(file_path):
     with open(file_path, 'r') as file:
         data = file.read()
     
-    python_splitter = RecursiveCharacterTextSplitter.from_language(
-        language=Language.PYTHON, chunk_size=50, chunk_overlap=0
-    )
+    # python_splitter = RecursiveCharacterTextSplitter.from_language(
+    #     language=Language.PYTHON, chunk_size=50, chunk_overlap=0
+    # )
 
-    python_docs = python_splitter.create_documents([data])
-    return data, python_docs
+    # python_docs = python_splitter.create_documents([data])
+    return data
 
 
-def load_vector_db():
-    """Load or create the vector database."""
-    # Pull the embedding model if not already available
-    ollama.pull(EMBEDDING_MODEL)
+def create_retriever():
+    """Create a retriever."""
 
     embedding = OllamaEmbeddings(model=EMBEDDING_MODEL, temperature=0)
 
-    if os.path.exists(PERSIST_DIRECTORY):
+    data_file_path = resource_filename(__name__, 'data-new.txt')
+    tskit_file_path =  resource_filename(__name__, 'tskit-vector')
+
+    if os.path.exists(tskit_file_path):
         print("Loading existing vector database...")
-        vector_db = FAISS.load_local(PERSIST_DIRECTORY, embedding, allow_dangerous_deserialization=True)
+        vector_db = FAISS.load_local(tskit_file_path, embedding, allow_dangerous_deserialization=True)
         print("Vector database loaded.")
-        logging.info("Loaded existing vector database.")
     else:
         print("Creating new vector database...")
         # Load and process the text document
-        data, _ = read_document(DOC_PATH)
+        data = read_document(data_file_path)
         python_splitter = character.RecursiveCharacterTextSplitter.from_language(
             language=base.Language.PYTHON
             )
         python_splits = python_splitter.split_text(data)
         vector_db = FAISS.from_texts(python_splits, embedding)
-        vector_db.save_local(PERSIST_DIRECTORY)
-        logging.info("Vector database created and persisted.")
+        vector_db.save_local(tskit_file_path)
 
-    return vector_db
 
-def create_retriever(vector_db, llm):
-    """Create a multi-query retriever."""
-    QUERY_PROMPT = PromptTemplate(
-        input_variables=["question"],
-        template="""You are an AI language model assistant. Your task is to generate five
-different versions of the given user question to retrieve relevant documents from
-a vector database. By generating multiple perspectives on the user question, your
-goal is to help the user overcome some of the limitations of the distance-based
-similarity search. Provide these alternative questions separated by newlines.
-Original question: {question}""",
+    retriever = vector_db.as_retriever(
+        search_type="similarity",  # Also test "similarity", "mmr"
+        search_kwargs={"k": 5}
     )
 
-    print("in create_retriever")
-    retriever = MultiQueryRetriever.from_llm(
-        vector_db.as_retriever(), llm, prompt=QUERY_PROMPT
-    )
-    print(retriever)
-    print("retriever created")
-    logging.info("Retriever created.")
-    # retriever = vector_db.as_retriever(
-    #         search_type="similarity",  # Also test "similarity", "mmr"
-    #         search_kwargs={"k": 5}
-    #         )
     return retriever
 
 
-def create_chain(retriever, llm):
+def create_chain(retriever, llm, input_query):
     """Create the chain with preserved syntax."""
-    # RAG prompt
-#     template = """Answer the question based ONLY on the following context:
-# {context}
-# Question: {question}
-# """
+
+    # Retriever model
+    docs = retriever.invoke(input_query)
+
+    # docs = retriever.get_relevant_documents(question)
+    context = "\n".join([doc.page_content for doc in docs])
 
     template = """You are a coding generator with expertise in using tskit toolkit for analysing tree-sequences. \n 
                     Here is a relevant set of tskit documentation:  \n ------- \n  {context} \n ------- \n Use the tskit module to answer the user 
@@ -131,51 +113,47 @@ def create_chain(retriever, llm):
 
     print("in create_chain")
     chain = (
-        {"context": retriever, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
+        prompt
+        | llm 
     )
     print("chain created")
 
-    logging.info("Chain created with preserved syntax.")
-    return chain
+    response = chain.invoke({"context": context, "question": input_query})
+    
+    return response
+
+
+def setup_llm_and_retriever():
+    # Initialize the language model
+    print("Loading language model...")
+    llm = ChatOllama(model=MODEL_NAME, temperature=0)
+
+    # Load the vector database
+    print("Loading vector database and making retriever...")
+    retriever = create_retriever()
+
+    return llm, retriever 
+
+llm, retriever = setup_llm_and_retriever()
 
 
 def main():
+    
     st.title("Ollama Llama 3.2 - Code Generator RAG Test")
 
     # User input
     user_input = st.text_input("Enter your question:", "")
 
+
     if user_input:
         with st.spinner("Generating response..."):
             try:
-                # Initialize the language model
-                print("Loading language model...")
-                llm = ChatOllama(model=MODEL_NAME)
-
-                # Load the vector database
-                print("Loading vector database...")
-                vector_db = load_vector_db()
-                if vector_db is None:
-                    st.error("Failed to load or create the vector database.")
-                    return
-
-                # Create the retriever
-                print("Creating retriever...")
-                retriever = create_retriever(vector_db, llm)
-
                 # Create the chain
                 print("Creating chain...")
-                chain = create_chain(retriever, llm)
-
-                # Get the response
-                print("Invoking chain...")
-                response = chain.invoke(input=user_input)
+                response = create_chain(retriever, llm, user_input)
 
                 st.markdown("**Assistant:**")
-                st.write(response)
+                st.write(response.content)
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
     else:
