8c8e71deb6699466ea7e3ca9d554e9c28cff8d43
pratikkatte <pratikkatte7@gmail.com>
2025-12-16 10:30:50 -0800

    code cleanup

--- launch.py
+++
@@ -1,9 +0,0 @@
-from lorax.interface import main
-if __name__ == '__main__':
-    INTERFACE = True
-    if INTERFACE:
-        # interface()
-        main()
-    else:
-        print("No interface")
-        # chat_interface()
